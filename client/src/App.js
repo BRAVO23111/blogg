d88129bb03929c9b5ee6de739f8625f9e6ca137e
DEBANJAN MUKHERJEE <debanjanmukherjee@DEBANJANs-MacBook-Air.local>
2023-08-24 21:49:35 +0530

    pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-
-import './App.css';
-
+import "./App.css";
+import Header from "./Header";
+import Post from "./Post";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./IndexPage";
+import Login from "./Login";
+import Register from "./Register";
 function App() {
   return (
-    <main>
-      <header>
-        <a href="">
-        My Blog
-        </a>
-        <a href="">
-        Login
-        </a>
-        <a href="">
-         Register         </a>
-        </header>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div className="entries">
+    <div className="image">
+      <img src="https://pbs.twimg.com/media/F3zpnU3bMAAXJ9O?format=jpg&name=medium"></img>
+    </div>
+    <div className="text">
+      Indian spacecraft Chandrayaan-3's Vikram lander on Friday successfully
+      underwent its first deboosting manoeuvre, said Indian Space Research
+      Organisation (ISRO). The authorities asserted that the lander module's
+      health was normal. "LM successfully underwent a deboosting operation
+      that reduced its orbit to 113 km x 157 km," said ISRO in a X (formerly
+      Twitter) post.
+    </div>
+  </div>
+  )
+}
+
+export default Post
