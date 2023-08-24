49feee75819fdf23862fbead92c7b1a06807ec91
DEBANJAN MUKHERJEE <debanjanmukherjee@DEBANJANs-MacBook-Air.local>
2023-08-25 01:25:10 +0530

    more changes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.json("got it")
-})
 
-app.listen(4000,(req,res)=>{
-    console.log("server at 4000");
-})
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  res.json({ requestData: { username, password } });
+});
+
+app.listen(4000, (req, res) => {
+  console.log("server at 4000");
+});
+
+////mongodb+srv://blog:7MqtGudi8a3zv5Ar@cluster0.1ksvgrb.mongodb.net/?retryWrites=true&w=majority
--- client/src/Page/Register.js
+++ client/src/Page/Register.js
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+  const [username,setusername] =useState('')
+  const [password,setpassword] =useState('')
+  async function register(e){
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:4000/register", {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response.status === 200) {
+        alert('Registration successful');
+      } else {
+        alert('Registration failed');
+      }
+    } catch (error) {
+      console.error("Error during registration:", error);
+      alert('An error occurred during registration');
+    }
+  }
   return (
     <div class="login-container">
-    <form class="login-form">
+    <form class="login-form" onSubmit={register}>
     <h2>Register</h2>
       <label for="username">Username:</label>
-      <input type="text" id="username" name="username" required/>
+      <input type="text" id="username" name="username" required value={username} onChange={(e)=>setusername(e.target.value)}/>
       <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required/>
+      <input type="password" id="password" name="password" required value={password} onChange={(e)=>setpassword(e.target.value)}/>
       <button type="submit">Login</button>
     </form>
   </div>
   )
 }
 
+
 export default Register
+
+
+//7MqtGudi8a3zv5Ar
+//mongodb+srv://blog:7MqtGudi8a3zv5Ar@cluster0.1ksvgrb.mongodb.net/?retryWrites=true&w=majority
