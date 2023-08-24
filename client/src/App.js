663e99d5194f9df49c71c5537ef405de29ead466
DEBANJAN MUKHERJEE <debanjanmukherjee@DEBANJANs-MacBook-Air.local>
2023-08-24 15:51:34 +0530

    first changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <main>
-      grerr
+      <header>
+        <a href="">
+        My Blog
+        </a>
+        <a href="">
+        Login
+        </a>
+        <a href="">
+         Register         </a>
+        </header>
     </main>
   );
 }
