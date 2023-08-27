181260bde2cb5384e2777f7658517334fe1f9c22
DEBANJAN MUKHERJEE <debanjanmukherjee@DEBANJANs-MacBook-Air.local>
2023-08-27 21:15:10 +0530

    createpost done

--- api/index.js
+++ api/index.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongoose=require('mongoose');
-const User = require('./models/User')
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
-
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const uploadmiddleware = multer({ dest: "uploads" });
+const fs = require("fs");
 const salt = bcrypt.genSaltSync(10);
 const secret = "hasbdfcbsbdfibsdbhcbsdhjcb";
-app.use(cookieParser()) ;
+const PostUser = require("../api/models/PostUser");
+app.use(cookieParser());
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 //database connection
- mongoose.connect('mongodb+srv://blog:sgFdSqpCYAq5wW6T@cluster0.exijc3f.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true})
-
-
+mongoose.connect(
+  "mongodb+srv://blog:sgFdSqpCYAq5wW6T@cluster0.exijc3f.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 
 app.use(express.json());
 // app.use(express.static(__dirname));
@@ -26,53 +30,64 @@ app.use(express.json());
 //login get request chck
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const userDoc = await User.create({username,password:bcrypt.hashSync(password,salt)})
+  const userDoc = await User.create({
+    username,
+    password: bcrypt.hashSync(password, salt),
+  });
 
-  res.json(userDoc); 
+  res.json(userDoc);
 });
 
-
-app.post('/login', async (req,res) => {
-  const {username,password} = req.body;
-  const userDoc = await User.findOne({username});
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
   const passOk = bcrypt.compareSync(password, userDoc.password);
   if (passOk) {
     // logged in
-    jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err;
-      res.cookie('token',token).json({
-        id:userDoc._id,
+      res.cookie("token", token).json({
+        id: userDoc._id,
         username,
       });
     });
   } else {
-    res.status(400).json('wrong credentials');
+    res.status(400).json("wrong credentials");
   }
 });
 
-
-app.get('/profile', (req,res) => {
-  const {token} =req.cookies;
-  jwt.verify(token,secret,{},(err,info)=>{
-    if (err) throw err ;
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
     res.json(info);
-  })
+  });
 });
 
-app.post('/logout', (req,res) => {
-  res.cookie('token', '').json('ok');
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
 });
 
+app.post("/post", uploadmiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newpath = path + "." + ext;
+  fs.renameSync(path, newpath);
+  const { title, summary, content } = req.body;
+  const PostDoc = await PostUser.create({
+    title,
+    summary,
+    content,
+    cover: newpath,
+  });
+  res.json(PostDoc);
+});
 
 app.listen(4000, (req, res) => {
   console.log("server at 4000");
 });
 
-
-
-
-
 //
-
 
 //D6VuXLeNQcVwDkCS
--- client/src/Components/Post.js
+++ client/src/Components/Post.js
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import React from 'react';
 const Post = () => {
   return (
     <div className="entries">
